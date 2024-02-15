9df4e32ecba9c48e223db0623f5481558b01774a
Cyrill Burgener <cyrill.burgener@gmail.com>
2024-02-15 21:14:15 +0100

    Add shoelaces

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import * as contentful from "contentful";
 import { remark } from "remark";
 import remarkHtml from "remark-html";
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import "@shoelace-style/shoelace/dist/components/card/card.js";
+import "@shoelace-style/shoelace/dist/components/breadcrumb/breadcrumb.js";
 
-console.log(contentful);
 const client = contentful.createClient({
-  space: "6d4vhsbxh0yj",
-  environment: "master",
-  accessToken: "6S26NCjEdWLehauOCnlaijvcbjfd3gashEfh4Bnwjpc",
+    space: "6d4vhsbxh0yj",
+    environment: "master",
+    accessToken: "6S26NCjEdWLehauOCnlaijvcbjfd3gashEfh4Bnwjpc",
 });
 
-const entry = await client.getEntry("7CHb54awH0gjxM69qIrUG3");
+const fetchContent = async (entryId: string) => {
+    try {
+        const entry = await client.getEntry(entryId);
+        const renderedContent = await remark().use(remarkHtml).process(entry.fields.content as string);
+        console.log(renderedContent.toString());
+    } catch (error) {
+        console.error("Error processing content:", error);
+    }
+};
 
-const renderedContent = await remark()
-  .use(remarkHtml)
-  .process(entry.fields.content as string);
-console.log(renderedContent.toString());
+fetchContent("7CHb54awH0gjxM69qIrUG3");
