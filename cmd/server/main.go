94fda07914753e4cf570d60062e278babc9e86e5
Cyrill Burgener <cyrill.burgener@gmail.com>
2025-08-04 22:55:46 +0200

    Keep current page highlighted in navbar

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"html/template"
 	"io/fs"
 	"log/slog"
@@ -29,7 +30,28 @@ func loadTemplates() (*template.Template, error) {
 		return nil
 	})
 
-	return template.ParseFiles(tmplFiles...)
+	tmpl := template.New("main")
+	tmpl.Funcs(template.FuncMap{
+		"dict": dict,
+	})
+
+	return tmpl.ParseFiles(tmplFiles...)
+}
+
+func dict(values ...any) (map[string]any, error) {
+	if len(values)%2 != 0 {
+		return nil, errors.New("invalid dict call: number of arguments must be even")
+	}
+
+	dict := make(map[string]any, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			return nil, errors.New("dict keys must be strings")
+		}
+		dict[key] = values[i+1]
+	}
+	return dict, nil
 }
 
 func main() {
